68339b771495d991a1f582d9f93200314040e2fb
John7794 <103057422+John7794@users.noreply.github.com>
2022-08-30 18:32:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -28,16 +28,16 @@ function testGS(e) {
     const url = 'https://script.google.com/macros/s/AKfycbzOSHQIq69wpQtBqGOiEkm--NZJ-Po8vs6LaaFAjGwfV2c9o_l5Y0mJJIvqG6FTTNSp/exec';
 
     fetch(url, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'no-cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
+        method: 'POST',
+        mode: 'no-cors',
+        cache: 'no-cache',
+        credentials: 'omit',
         headers: {
         'Content-Type': 'application/json'
         // 'Content-Type': 'application/x-www-form-urlencoded',
         },
-        redirect: 'follow', // manual, *follow, error
-        // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        redirect: 'follow',
+        // referrerPolicy: 'no-referrer',
         body: JSON.stringify(body)
     });
 }
