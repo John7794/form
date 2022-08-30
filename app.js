5121ba22473224286d318a60af2f1e792ec39222
John7794 <103057422+John7794@users.noreply.github.com>
2022-08-30 18:27:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
 function testGS(e) {
 
     e.preventDefault();
+    console.log();
+    const body = {};
+    var len = this.elements.length;
+    var props = [
+       'theme_report',
+        'name_report',
+        'surname',
+        'name',
+        'patronymic',
+        'job',
+        'job_post',
+        'degree',
+        'rank',
+        'address',
+        'phone',
+        'email'
+    ]
+
+    for ( var i = 0; i < len - 1; i++ ) {
+        body[props[i]] = this.elements[i].value;
+    }
+
+    console.log(body);
 
     const url = 'https://script.google.com/macros/s/AKfycbzOSHQIq69wpQtBqGOiEkm--NZJ-Po8vs6LaaFAjGwfV2c9o_l5Y0mJJIvqG6FTTNSp/exec';
 
-    fetch(url)
-        .then(d => d.json())
-        .then(d => {
-            document.getElementById('app').textContent = d[0].status;
-            alert(d[0].status);
-        });
+    fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        // mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(body)
+    });
 }
 
-document.getElementById('btn').addEventListener('click', testGS);
+document.getElementById('form').addEventListener('submit', testGS);
